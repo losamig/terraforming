e158bbff67438358821d7ffd1915f7d3c615a846
miguelgon <miguelgon@microsoft.com>
2022-08-16 08:52:33 +0200

    splitting files

--- basic/output.tf
+++ basic/output.tf
@@ -1,12 +1,3 @@
 output "resource_group_name" {
   value = azurerm_resource_group.speedy.name
 }
-
-output "speedy1_public_ip_address" {
-  value = azurerm_linux_virtual_machine.speedy1.public_ip_address
-}
-
-output "tls_private_key" {
-  value     = tls_private_key.example_ssh.private_key_pem
-  sensitive = true
-}
