4da6bb27975898bbd4019b97e9ca9f97547337c4
miguelgon <miguelgon@microsoft.com>
2022-08-19 13:47:57 +0200

    adding acr

--- basic/acr.tf
+++ basic/acr.tf
@@ -1,8 +1,8 @@
 # Creating an Azure Container Registry (ACR)
 # https://docs.microsoft.com/en-us/azure/container-registry/
 
-resource "azurerm_container_registry" "acr" {
-  name                     = "acr"
+resource "azurerm_container_registry" "speedy" {
+  name                     = "acrspeedy"
   location                 = azurerm_resource_group.speedy.location
   resource_group_name      = azurerm_resource_group.speedy.name
   sku                      = "Basic"
--- basic/networking.tf
+++ basic/networking.tf
@@ -37,3 +37,8 @@ resource "azurerm_subnet" "speedy" {
   virtual_network_name = azurerm_virtual_network.speedy.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+resource "azurerm_subnet_network_security_group_association" "speedy" {
+  subnet_id                 = azurerm_subnet.speedy.id
+  network_security_group_id = azurerm_network_security_group.speedy.id
+}
